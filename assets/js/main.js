7f27567b52a5b736e3fb59aeafc480ab124add4b
rich_sam7 <rahelsamson953@gmail.com>
2024-09-27 00:55:29 +0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// Initialize AOS (Animate on Scroll)
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
+        duration: 1200,  // Animation duration in milliseconds
+        once: true,      // Animation should happen only once while scrolling down
+    });
+});
